4a88ea1392932d38a78016b075d53d4a4dcc1982
Jeff May <admin@irongatestrategic.com>
2026-05-18 18:45:20 -0500

    fix: remove link-fixer-backups from tracking

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/llms.txt");
 
   // Date filters
   eleventyConfig.addFilter("readableDate", dateObj => {
